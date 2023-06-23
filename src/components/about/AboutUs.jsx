f80cc3d56ba345c4cca5c29bebae339aeb53f72c
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-23 09:02:26 -0700

    getstarted removed

--- src/components/about/AboutUs.jsx
+++ src/components/about/AboutUs.jsx
@@ -8,9 +8,7 @@ const AboutUs = () => (
       <p>AutoPy offers comprehensive programs covering DevOps, Agile/Scrum, GitHub, Co-Pilot, ChatGPT, architecture, design patterns, and TDD. Participants gain expertise in streamlining software development processes, fostering collaboration, and driving efficiency. They learn agile methodologies and the Scrum framework for faster and adaptive product delivery. Our training also covers GitHub for version control and collaboration. Additionally, participants explore AI capabilities with Co-Pilot and ChatGPT. They acquire skills in architecture design, utilizing modular and scalable systems, and design patterns for reusable solutions. Lastly, our TDD training promotes a test-first approach, ensuring code correctness and maintainability. Our training empowers professionals to thrive in the dynamic tech landscape and drive innovation.
      </p>
     </div>
-    <div className="auto__about-btn">
-      <button type="button">Get Started</button>
-    </div>
+  
   </div>
 );
 
