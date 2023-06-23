10ecdff5ebe4e9f2e243225ae675b9549871810a
Anjum <88701810+Anjum7866@users.noreply.github.com>
2023-06-23 06:23:34 -0700

    o

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import './header.css';
 const Header = () => (
   <div className="gpt3__header section__margin" id="home">
     <div className="gpt3__header-content">
-      <h1 className="gradient__text">Institute of Devops and Project Management</h1>
+      <h1 className="gradient__text">Institute of DevOps and Project Management</h1>
      </div>
 
   
